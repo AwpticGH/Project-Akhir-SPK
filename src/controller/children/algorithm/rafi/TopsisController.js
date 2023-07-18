e562908fd310621217b3467be11caccdc33abcbf
Rafi Fajar Sulaiman <95056688+AwpticGH@users.noreply.github.com>
2023-07-18 07:44:15 +0700

    Update TopsisController.js

--- src/controller/children/algorithm/rafi/TopsisController.js
+++ src/controller/children/algorithm/rafi/TopsisController.js
@@ -90,7 +90,7 @@ class TopsisController extends BaseController {
                     // set scores
                     topsisModel.scores[j] = {};
                     topsisModel.scores[j].criteria = criterias[j].name;
-                    topsisModel.scores[j].score = matrixScoreModels[index][j].score;
+                    topsisModel.scores[j].score = Number.parseInt(matrixScoreModels[index][j].score);
                 }
             }
 
